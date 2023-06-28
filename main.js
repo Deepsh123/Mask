3204b014a9e33294aec4a5d9de3b51ee77ce9374
Deepsh123 <105092065+Deepsh123@users.noreply.github.com>
2023-06-27 19:04:38 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+prediction_1 = "";
+Webcam.set ({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach(  '#camera' );
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML= '<img id = "captured_image" src = "'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Vo_ge2oBv/model.json',modelLoaded);
+function modelLoaded() {
+    console.log('Model loaded!');
+}
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is"+prediction_1;
+var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+synth.speak(utterThis);
+}
+function check() {
+take_snapshot();
+    img = document.getElementById('captured_image')
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+if (error) {
+    console.error(error);
+} else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(2) + "%";
+   
+    }
+}
